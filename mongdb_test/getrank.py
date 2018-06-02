bddf23dfcef028753d5b2c2c074a4c01bde26449
jesns smith <smithjesns@gmail.com>
2018-06-02 23:21:55 +0800

    mongdb test

---
+++ mongdb_test/getrank.py
@@ -0,0 +1,47 @@
+
+# -*- coding: utf-8 -*-
+
+import sys
+from pymongo import MongoClient
+
+
+def get_rank(user_id):
+    client = MongoClient()
+    db = client.shiyanlou
+    contests = db.contests
+
+    user_data = {}
+    user=[]
+    user_ids=set()
+    for contest in contests.find():
+        user_ids.add(contest.get('user_id'))
+        if user_data.get(contest.get('user_id')):
+            user_data[contest.get('user_id')]['score'] += contest.get('score')
+            user_data[contest.get('user_id')]['submit_time'] += contest.get('submit_time')
+        else:
+            user_data[contest.get('user_id')]={'score' : contest.get('score'), 'submit_time' : contest.get('submit_time')}
+
+    for i,j in user_data.items():
+        user.append((i,j['score'],j['submit_time']))
+    sort_user = sorted(user,key=lambda x:(-x[1] ,[2]))
+
+    for i,j in enumerate(sort_user):
+        user_data[j[0]]['rank'] = i + 1
+
+    if user_id not in user_ids:
+        return "Parameter Error"
+    # return sort_user
+    return user_data[int(user_id)]['rank'], user_data[int(user_id)]['score'], user_data[int(user_id)]['submit_time']
+if __name__ == '__main__':
+    
+    if len(sys.argv[1:]) > 1:
+        print("Parameter Error")
+    else:
+        try:
+
+            user_id = int(sys.argv[1])
+            userdata = get_rank(user_id)
+            print(userdata)
+        except:
+            print("Parameter Error")
+
